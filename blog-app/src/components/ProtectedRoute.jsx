8b95d36599450964f3e831b73ce2d3855f669794
ionescuea <60351947+ionescuea@users.noreply.github.com>
2025-06-13 18:21:11 +0100

    Fix reload page

--- blog-app/src/components/ProtectedRoute.jsx
+++ blog-app/src/components/ProtectedRoute.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
-import { Navigate } from 'react-router-dom';
+import { Navigate, useLocation } from 'react-router-dom';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { auth } from '../firebase';
 
 const ProtectedRoute = ({ children }) => {
   const [user, loading] = useAuthState(auth);
+  const location = useLocation();
 
 if (loading) {
   return (
